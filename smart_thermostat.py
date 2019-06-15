53ee583e8c0f406f823c97c0ed5ffb24edd4f805
Unknown <michael.r.hooi@gmail.com>
2019-06-15 17:27:14 -0400

    Add states and function to pull humidity and temperature

--- smart_thermostat.py
+++ smart_thermostat.py
@@ -1,4 +1,7 @@
+from enum import Enum
+
 import RPi.GPIO as GPIO
+import Adafruit_DHT as DHT
 
 GPIO.setmode(GPIO.BCM)
 
@@ -8,30 +11,48 @@ for i in pin_list:
   GPIO.setup(i, GPIO.OUT)
   GPIO.output(i, GPIO.HIGH)
 
+class State(Enum):
+  OFF = 1
+  COOL = 2
+  HEAT = 3
+  AUXHEAT = 4
+  FAN = 5
 
 class Thermostat():
   def __init__(self):
-    pass
+    self.state = State.OFF
+    self.get_humidity_and_temperature()
 
   def turn_off(self):
     for i in pin_list:
       GPIO.output(i, GPIO.HIGH)
+    self.state = State.OFF
 
   def cool_on(self):
     self.turn_off()
     GPIO.output(2, GPIO.LOW)
     GPIO.output(3, GPIO.LOW)
+    self.state = State.COOL
 
   def heat_on(self):
     self.turn_off()
     GPIO.output(2, GPIO.LOW)
     GPIO.output(4, GPIO.LOW)
+    self. state = State.HEAT
 
   def aux_heat_on(self):
     self.turn_off()
     GPIO.output(2, GPIO.LOW)
     GPIO.output(17, GPIO.LOW)
+    self.state = State.AUXHEAT
 
   def fan_on(self):
     self.turn_off()
     GPIO.output(2, GPIO.LOW)
+    self.state = State.FAN
+
+  def get_humidity_and_temperature(self):
+    self.humidity, self.temperature = DHT.read_retry(DHT.DHT22, 27)
+
+
+  
